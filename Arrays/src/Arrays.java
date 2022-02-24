74711a828ec4be3f9e3a6c52c22a4f5c9122b385
Dino <dm3986647@gmail.com>
2022-02-24 13:54:48 +0100

    Arrays

---
+++ Arrays/src/Arrays.java
@@ -0,0 +1,17 @@
+
+public class Arrays {
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcome to our program!");
+		
+		
+		String backpack[] = {"Shotgun", "Assault rifle", "Sniper"};
+		String zombies[] = {"Close range zombie", "Mid range zombie", "long range zombie"};
+		
+		System.out.println(backpack[2]);
+		int	numbersZombiesHate[] = { 4,  90, 73, 12};
+		System.out.println(numbersZombiesHate[2]);
+	}
+
+}
