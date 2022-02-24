4597cf1478a0671e7e4c573bf48a4805f11a5dbc
Dino <dm3986647@gmail.com>
2022-02-24 13:49:53 +0100

    Public, private, protected i default

---
+++ AccessMod/src/package1/Access1.java
@@ -0,0 +1,18 @@
+package package1;
+
+public class Access1 {
+
+		//public int hours = 3;
+		//public int minutes = 47;
+	
+	//	private int hours = 3;
+	//	private int minutes = 47;
+		
+		protected int hours = 3;
+		protected int minutes = 47;
+		
+		public int getHours() {
+			return hours;
+			
+		}
+ }
