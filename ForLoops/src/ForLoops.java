1e968de34426f341126df2c6dcbc6e4e90ef29eb
Dino <dm3986647@gmail.com>
2022-02-25 09:58:33 +0100

    For loops

---
+++ ForLoops/src/ForLoops.java
@@ -0,0 +1,19 @@
+
+public class ForLoops {
+
+	public static void main(String[] args) {
+
+		for(int i=0; i < 10; i++){
+		System.out.println("I love dogs");
+		}
+		
+		
+		int[] grades = {98, 100, 50, 58};
+		for(int i = 0; i < grades.length; i++){
+			System.out.println(grades[i]);
+			
+			
+		}
+		
+	}
+}
