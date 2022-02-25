5d19b5e7d96ab8787edaaa249cb695e78ab9df24
Dino <dm3986647@gmail.com>
2022-02-25 10:39:09 +0100

    Metode

---
+++ Methods/src/Methods.java
@@ -0,0 +1,33 @@
+
+public class Methods {
+
+	public static void main(String[] args) {
+		 
+		welcome();
+		multiply(5,5);
+		divide(20,5); 
+
+	}
+	
+	public static void welcome(){
+		System.out.println("welcome to our calculator");
+		
+		
+	}
+	
+
+	public static void multiply(int a, int b){
+		
+		System.out.println(a*b);
+		
+		
+	}
+
+	public static void divide(int a, int b) {
+		
+		System.out.println(a / b);
+		
+	}
+	
+	
+}
