1de58775710108cada4c365957a33d1bb4c52464
Dino <dm3986647@gmail.com>
2022-02-25 10:36:56 +0100

    2D arrays

---
+++ LotteryTicket/src/LotteryTicket.java
@@ -0,0 +1,22 @@
+
+public class LotteryTicket {
+
+	public static void main(String[] args) {
+
+		int [][] lotteryCard = {{ 20,15,17},
+								{8,7,19},
+								{5,9,8}
+		};
+		
+		
+		System.out.println(lotteryCard[0][0 ]);
+		
+		
+		for( int i = 0; i<3; i++ ){
+			
+			System.out.println(lotteryCard[i][i]);
+				 	
+		}
+	} 
+
+}
