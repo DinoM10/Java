aefda70d0f0a71d4fadd6d85ca840b5ed68fa170
Dino <dm3986647@gmail.com>
2022-02-25 09:40:50 +0100

    Trenutno vrijeme

---
+++ CurrentTime/src/CurrentTime.java
@@ -0,0 +1,23 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class CurrentTime {
+
+	public static void main(String[] args) {
+			
+	Date currentDate = new Date();
+	System.out.println(currentDate);
+	
+	
+	SimpleDateFormat timeFormat = new SimpleDateFormat("hh:mm:ss");
+	System.out.println(timeFormat.format(currentDate));
+	
+	SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+	System.out.println(dateFormat.format(currentDate));
+	
+	SimpleDateFormat dayOfTheWeek = new SimpleDateFormat("EEEE");
+	System.out.println(dayOfTheWeek.format(currentDate));
+
+	}
+
+}
