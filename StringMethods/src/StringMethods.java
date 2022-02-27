a39ede3e9b76aff30e96798c3ccb5cd7379cc2f2
Dino <dm3986647@gmail.com>
2022-02-27 11:47:54 +0100

    String metode

---
+++ StringMethods/src/StringMethods.java
@@ -0,0 +1,14 @@
+
+public class StringMethods {
+
+	public static void main(String[] args) {
+		
+		String name = "Billy Bob Joe";
+		String name2 = "Alex";
+		System.out.println("Name:"+ name);
+		System.out.println(name.replace('B', ' '));
+		System.out.println(name.replace('J', ' '));
+
+	}
+
+}
