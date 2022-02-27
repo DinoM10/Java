0264d6c51f44b9805b0f81b3de75accb528c6648
Dino <dm3986647@gmail.com>
2022-02-27 12:02:22 +0100

    Varijable

---
+++ Varijable/src/Varijable.java
@@ -0,0 +1,14 @@
+
+public class Varijable {
+
+	public static void main(String[] args) {
+		int x = 123;
+		double y = 3.14;
+		boolean z = true;
+		char symbol = '@';
+		String name = "Ime";
+		
+		System.out.println("Hello "+name);
+	}
+
+}
