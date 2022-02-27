42aa8010aedd62a0efc6abaf7d03423dbc696fad
Dino <dm3986647@gmail.com>
2022-02-27 11:46:48 +0100

    Static

---
+++ Static/src/friend.java
@@ -0,0 +1,17 @@
+
+public class friend {
+
+	String name;
+	static int numberOfFriends;
+	
+	
+	friend(String name){
+		this.name = name;
+		 numberOfFriends++;
+		 }
+	
+	static void displayFriends(){
+		
+		System.out.println("You have " + numberOfFriends + " friends");
+	}
+}
---
+++ Static/src/main.java
@@ -0,0 +1,14 @@
+
+public class main {
+
+	public static void main(String[] args) {
+
+		friend friend1 = new friend("Spongebob");
+		friend friend2 = new friend("Patrick");
+
+		System.out.println(friend.numberOfFriends);
+		friend.displayFriends();
+		
+	}
+
+}
