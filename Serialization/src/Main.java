73f6fd509db32ea1863c2641b6c37439412187df
Dino <dm3986647@gmail.com>
2022-03-01 12:31:51 +0100

    Serialization

---
+++ Serialization/src/Main.java
@@ -0,0 +1,24 @@
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+
+		User user = new User();
+		
+		user.name = "Dino";
+		user.password = "lozinka";
+		
+		FileOutputStream fileOut = new FileOutputStream("UserInfo.ser");
+		ObjectOutputStream out = new ObjectOutputStream(fileOut);
+		out.writeObject(user);
+		out.close();
+		fileOut.close();
+		
+		System.out.println("Object info saved!");
+		
+		long serialVersionUID = ObjectStreamClass.lookup(user.getClass()).getSerialVersionUID();
+		System.out.println(serialVersionUID);
+	}
+
+}
